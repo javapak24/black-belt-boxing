ec0449ad5047953a28f3aa487885829618a52cad
javapak24 <ajava2424@gmail.com>
2025-03-23 16:02:31 -0400

    responsiveness of header

--- src/components/SuperHeader.js
+++ src/components/SuperHeader.js
@@ -1,40 +1,41 @@
-import React from 'react'
-import pic from '../blackbelt_logo.jpg'
+import React from 'react';
+import pic from '../blackbelt_logo.jpg';
 
 import {
-    Container,
-    Grid,
-    GridColumn,
-    GridRow,
-    Header,
-    Image,
-    Segment,
-  } from 'semantic-ui-react'
-import HeaderContact from './HeaderContact'
-import HeaderSchedule from './HeaderSchedule'
+  Container,
+  Grid,
+  GridColumn,
+  GridRow,
+  Header,
+  Image,
+  Segment,
+} from 'semantic-ui-react';
+import HeaderContact from './HeaderContact';
+import HeaderSchedule from './HeaderSchedule';
 
 export default function SuperHeader() {
   return (
     <div>
-      <Container text style={{ marginTop: '7em', margin: '10px' }}>
-      <Segment inverted>
-      <Header as='h1' style={{color: 'white'}}>Black Belt Boxing</Header>
-      </Segment>
-      <Grid columns={3} divided>
-            <GridRow columns='equal'>
-                <GridColumn>
-                  <HeaderSchedule/>
-                </GridColumn> 
-                <GridColumn>
-                  <Image src = {pic} size='medium'/>
-                </GridColumn>
-                <GridColumn>
-                  <HeaderContact/>
-                </GridColumn>
-            </GridRow>
+      <Container style={{ marginTop: '7em', margin: '10px' }}>
+        <Segment inverted style={{ width: '100%' }}>
+          <Header as="h1" style={{ color: 'white' }}>
+            Black Belt Boxing
+          </Header>
+        </Segment>
+        <Grid columns={3} divided style={{ width: '100%' }}>
+          <GridRow columns="equal">
+            <GridColumn>
+              <HeaderSchedule />
+            </GridColumn>
+            <GridColumn>
+              <Image src={pic} size="medium" />
+            </GridColumn>
+            <GridColumn>
+              <HeaderContact />
+            </GridColumn>
+          </GridRow>
         </Grid>
-    
       </Container>
     </div>
-  )
+  );
 }
