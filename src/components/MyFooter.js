c61592f2f314e5d58607012d772eb6c9ed1ac904
javapak24 <ajava2424@gmail.com>
2025-03-25 16:15:25 -0400

    adjustments to center footer contents on mobile

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -12,7 +12,14 @@ import {
 } from 'semantic-ui-react';
 
 const MyFooter = () => (
-  <div>
+  <div
+    class="footer-container"
+    // style={{
+    //   display: 'flex',
+    //   alignItems: 'center',
+    //   justifyContent: 'center',
+    // }}
+  >
     <Segment inverted vertical style={{ padding: '5em 0em' }}>
       <Container>
         <Grid divided inverted stackable>
@@ -25,45 +32,48 @@ const MyFooter = () => (
                   width: 'max-content',
                   overflow: 'auto',
                   textOverflow: 'ellipsis',
+                  margin: '0 auto',
                 }}
               >
-                <List>
-                  <ListItem>
-                    <ListIcon name="phone" />
-                    <ListContent>(774) 836-5924</ListContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon name="marker" />
-                    <ListContent>
-                      20 Baxter Road, Hyannis, MA, <br /> United States,
-                      Massachusetts
-                    </ListContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon name="mail" />
-                    <ListContent>
-                      <a href="mailto:blackbeltboxinginc@gmail.com">
-                        blackbeltboxinginc@gmail.com
-                      </a>
-                    </ListContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon name="linkify" />
-                    <ListContent>
-                      <a href="javapak24.github.io/black-belt-boxing/">
-                        blackbeltboxing.com
-                      </a>
-                    </ListContent>
-                  </ListItem>
-                  <ListItem>
-                    <ListIcon name="facebook" />
-                    <ListContent>
-                      <a href="https://www.facebook.com/p/Black-Belt-Boxing-Inc-61565732704741/">
-                        facebook.com
-                      </a>
-                    </ListContent>
-                  </ListItem>
-                </List>
+                <div>
+                  <List>
+                    <ListItem>
+                      <ListIcon name="phone" />
+                      <ListContent>(774) 836-5924</ListContent>
+                    </ListItem>
+                    <ListItem>
+                      <ListIcon name="marker" />
+                      <ListContent>
+                        20 Baxter Road, Hyannis, MA, <br /> United States,
+                        Massachusetts
+                      </ListContent>
+                    </ListItem>
+                    <ListItem>
+                      <ListIcon name="mail" />
+                      <ListContent>
+                        <a href="mailto:blackbeltboxinginc@gmail.com">
+                          blackbeltboxinginc@gmail.com
+                        </a>
+                      </ListContent>
+                    </ListItem>
+                    <ListItem>
+                      <ListIcon name="linkify" />
+                      <ListContent>
+                        <a href="javapak24.github.io/black-belt-boxing/">
+                          blackbeltboxing.com
+                        </a>
+                      </ListContent>
+                    </ListItem>
+                    <ListItem>
+                      <ListIcon name="facebook" />
+                      <ListContent>
+                        <a href="https://www.facebook.com/p/Black-Belt-Boxing-Inc-61565732704741/">
+                          facebook.com
+                        </a>
+                      </ListContent>
+                    </ListItem>
+                  </List>
+                </div>
               </Segment>
             </Grid.Column>
             {/* <Grid.Column width={3}>
