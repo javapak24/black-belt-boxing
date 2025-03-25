864cd4ae26d5250f404edd469c5a856f7369e13c
javapak24 <ajava2424@gmail.com>
2025-03-25 18:11:17 -0400

    nav bar styling for responsiveness

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,33 +1,59 @@
-import React from 'react'
-import BBB from '../blackbelt_logo.jpg'
+import React from 'react';
+import BBB from '../blackbelt_logo.jpg';
 
-import {
-  Container,
-  Grid,
-  GridColumn,
-  GridRow,
-  Header,
-  Image,
-  Menu
-} from 'semantic-ui-react'
+import { Container, Image, Menu } from 'semantic-ui-react';
 
 export default function MyHeader() {
   return (
     <div>
       <Menu inverted>
-      <Container style={{ display: "flex", justifyContent: "space-between" , listStyle: "none", overflow: 'auto'}}>
-        <Menu.Item as='a' href ='#/' header>
-        <Image size='mini' src= {BBB} style={{ marginRight: '1.5em' }} />
-        BBB
-        </Menu.Item>
-        <Menu.Item as='a' href="#/">Home</Menu.Item>
-        <Menu.Item as='a' href="#/pay">Secure Pay</Menu.Item>
-        <Menu.Item as='a' href="#/about">About</Menu.Item>
-        <Menu.Item as='a' href="#/schedule">Schedule</Menu.Item>
-        <Menu.Item as='a' href="#/usaboxing">USA Boxing</Menu.Item>
-        <Menu.Item as='a' href="#/waiver">Waiver</Menu.Item>
-      </Container>
-    </Menu>
+        <Container
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center', // Ensures vertical alignment
+            listStyle: 'none',
+            overflow: 'auto',
+          }}
+        >
+          <Menu.Item
+            as="a"
+            href="#/"
+            header
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center', // Centers logo and text
+            }}
+          >
+            <Image size="mini" src={BBB} style={{ marginRight: '1.5em' }} />
+            BBB
+          </Menu.Item>
+          {[
+            'Home',
+            'Secure Pay',
+            'About',
+            'Schedule',
+            'USA Boxing',
+            'Waiver',
+          ].map((item, index) => (
+            <Menu.Item
+              key={index}
+              as="a"
+              href={`#/${item.toLowerCase().replace(' ', '')}`}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                textAlign: 'center',
+                flexGrow: 1, // Makes each item take equal space
+              }}
+            >
+              {item}
+            </Menu.Item>
+          ))}
+        </Container>
+      </Menu>
     </div>
-  )
+  );
 }
